b3df660926b87e1dcddeec614c0d47917f3b6f7b
Bob Vincent <pillarsdotnet@gmail.com>
2017-11-09 16:41:40 -0500

    Avoid unnecessary inheritance.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,12 @@
-class psad::install inherits psad::params {
-  package { $psad::install::package:
+class psad::install {
+  include psad
+  package { $psad::package:
     ensure => present,
     notify => Exec['psad_signature_update']
   }
 
   exec { 'psad_signature_update':
-    command     => $signature_update_command,
+    command     => $psad::signature_update_command,
     returns     => [0,1],
     refreshonly => true
   }
